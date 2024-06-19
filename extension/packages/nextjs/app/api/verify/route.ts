ded3f2ad3f11f33b227b88a0b8ed7259f479c265
Rinat <rinat@hey.com>
2024-06-19 22:20:38 +0200

    fix: formatting

--- extension/packages/nextjs/app/api/verify/route.ts
+++ extension/packages/nextjs/app/api/verify/route.ts
@@ -1,16 +1,10 @@
 import { NextResponse } from "next/server";
 import { recoverTypedDataAddress } from "viem";
-import {
-  EIP_712_DOMAIN,
-  EIP_712_TYPE,
-  VerifyRequestBody,
-  generateMessageToBob,
-} from "~~/utils/eip-712";
+import { EIP_712_DOMAIN, EIP_712_TYPE, VerifyRequestBody, generateMessageToBob } from "~~/utils/eip-712";
 
 export async function POST(req: Request) {
   try {
-    const { fromName, message, signature, signer } =
-      (await req.json()) as VerifyRequestBody;
+    const { fromName, message, signature, signer } = (await req.json()) as VerifyRequestBody;
 
     const recoveredAddress = await recoverTypedDataAddress({
       domain: EIP_712_DOMAIN,
@@ -21,18 +15,12 @@ export async function POST(req: Request) {
     });
 
     if (recoveredAddress !== signer) {
-      return NextResponse.json(
-        { error: "Recovered address does not match signer" },
-        { status: 401 }
-      );
+      return NextResponse.json({ error: "Recovered address does not match signer" }, { status: 401 });
     }
 
     return NextResponse.json({ message: "Success!" }, { status: 200 });
   } catch (e) {
     console.error(e);
-    return NextResponse.json(
-      { error: "Something went wrong" },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: "Something went wrong" }, { status: 500 });
   }
 }
--- extension/packages/nextjs/app/eip-712/page.tsx
+++ extension/packages/nextjs/app/eip-712/page.tsx
@@ -4,19 +4,13 @@ import { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import { SignTypedDataReturnType } from "viem/accounts";
 import { useAccount, useSignTypedData, useVerifyTypedData } from "wagmi";
-import {
-  EIP_712_DOMAIN,
-  EIP_712_TYPE,
-  VerifyRequestBody,
-  generateMessageToBob,
-} from "~~/utils/eip-712";
+import { EIP_712_DOMAIN, EIP_712_TYPE, VerifyRequestBody, generateMessageToBob } from "~~/utils/eip-712";
 import { getParsedError, notification } from "~~/utils/scaffold-eth";
 
 const Eip712: NextPage = () => {
   const { address: connectedAddress } = useAccount();
   const [signature, setSignature] = useState<SignTypedDataReturnType>();
-  const [signedTypedData, setSignedTypedData] =
-    useState<Record<string, unknown>>();
+  const [signedTypedData, setSignedTypedData] = useState<Record<string, unknown>>();
   const [isLoading, setIsLoading] = useState(false);
   const { signTypedDataAsync } = useSignTypedData();
 
@@ -113,15 +107,10 @@ const Eip712: NextPage = () => {
       <div className="flex flex-col gap-4 items-center text-center">
         <h1 className="text-2xl font-bold">EIP-712</h1>
         <div className="max-w-2xl">
-          EIP-712 defines a standard for hashing and signing typed structured
-          data in Ethereum. It enhances security and usability by enabling the
-          creation of more readable and secure signed messages, reducing the
-          risk of phishing attacks and user errors. For more details, visit the{" "}
-          <a
-            target="_blank"
-            href="https://eips.ethereum.org/EIPS/eip-712"
-            className="underline font-bold text-nowrap"
-          >
+          EIP-712 defines a standard for hashing and signing typed structured data in Ethereum. It enhances security and
+          usability by enabling the creation of more readable and secure signed messages, reducing the risk of phishing
+          attacks and user errors. For more details, visit the{" "}
+          <a target="_blank" href="https://eips.ethereum.org/EIPS/eip-712" className="underline font-bold text-nowrap">
             EIP-712 specification
           </a>
           .
@@ -136,53 +125,41 @@ const Eip712: NextPage = () => {
         </div>
         <div className="divider my-0" />
 
-        <div className="text-xl font-bold">
-          Send message to Bob using EIP-712
-        </div>
+        <div className="text-xl font-bold">Send message to Bob using EIP-712</div>
         <input
           placeholder="Your name"
           className="input input-bordered rounded-lg w-full sm:w-96"
           value={name}
-          onChange={(e) => setName(e.target.value)}
+          onChange={e => setName(e.target.value)}
         />
         <textarea
           placeholder="Your message"
           className="textarea textarea-bordered rounded-lg w-full sm:w-96 text-base"
           value={message}
-          onChange={(e) => setMessage(e.target.value)}
+          onChange={e => setMessage(e.target.value)}
         />
 
         <span>
-          Use Metamask or another wallet supporting
-          &quot;eth_signTypedData_v4&quot; to review typed data before signing.
+          Use Metamask or another wallet supporting &quot;eth_signTypedData_v4&quot; to review typed data before
+          signing.
         </span>
 
-        <button
-          className="btn btn-primary btn-sm"
-          onClick={signTypedData}
-          disabled={!connectedAddress}
-        >
+        <button className="btn btn-primary btn-sm" onClick={signTypedData} disabled={!connectedAddress}>
           Sign
         </button>
 
         <details className="collapse collapse-arrow bg-base-300 !max-w-full">
           <input type="checkbox" className="hidden" />
-          <summary className="collapse-title font-bold">
-            Current typed data
-          </summary>
+          <summary className="collapse-title font-bold">Current typed data</summary>
           <div className="collapse-content text-start">
-            <pre className="break-all">
-              {JSON.stringify(typedData, undefined, 2)}
-            </pre>
+            <pre className="break-all">{JSON.stringify(typedData, undefined, 2)}</pre>
           </div>
         </details>
 
         {signature && signedTypedData && (
           <details className="collapse collapse-arrow bg-base-300">
             <input type="checkbox" className="hidden" />
-            <summary className="collapse-title font-bold">
-              Signed typed data
-            </summary>
+            <summary className="collapse-title font-bold">Signed typed data</summary>
             <div className="collapse-content text-start">
               <pre>{JSON.stringify(signedTypedData, undefined, 2)}</pre>
             </div>
@@ -197,22 +174,13 @@ const Eip712: NextPage = () => {
         )}
 
         <div className="max-w-2xl">
-          To successfully verify current typed data and signed typed data must
-          be equal. Check <code className="font-bold">message.from.name</code>{" "}
-          and <code className="font-bold">message.contents</code>
+          To successfully verify current typed data and signed typed data must be equal. Check{" "}
+          <code className="font-bold">message.from.name</code> and <code className="font-bold">message.contents</code>
         </div>
-        <button
-          className="btn btn-primary btn-sm"
-          onClick={verifyOnFrontend}
-          disabled={!signature}
-        >
+        <button className="btn btn-primary btn-sm" onClick={verifyOnFrontend} disabled={!signature}>
           Verify (frontend)
         </button>
-        <button
-          className={`btn btn-primary btn-sm`}
-          onClick={verifyOnBackend}
-          disabled={!signature || isLoading}
-        >
+        <button className={`btn btn-primary btn-sm`} onClick={verifyOnBackend} disabled={!signature || isLoading}>
           {isLoading && <span className="loading loading-spinner" />}
           Verify (backend)
         </button>
