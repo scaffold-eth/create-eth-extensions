fd82574b82887ee757129487f71b1418e5dc03ca
Rinat <rinat@hey.com>
2024-07-22 19:37:46 +0200

    fix: update provider

--- extension/packages/nextjs/components/OnchainKitScaffoldProvider.tsx
+++ extension/packages/nextjs/components/OnchainKitScaffoldProvider.tsx
@@ -1,15 +1,14 @@
 import { PropsWithChildren } from "react";
 import { OnchainKitProvider } from "@coinbase/onchainkit";
+import { base } from "viem/chains";
 import { useAccount } from "wagmi";
 
 export const OnchainKitScaffoldProvider = ({ children }: PropsWithChildren) => {
   const { chain } = useAccount();
 
-  return chain ? (
-    <OnchainKitProvider chain={chain} apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}>
+  return (
+    <OnchainKitProvider chain={chain || base} apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}>
       {children}
     </OnchainKitProvider>
-  ) : (
-    children
   );
 };
