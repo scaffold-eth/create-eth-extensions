e9eda706cd6acfdb2d075767a169debc0a9156e9
Rinat <rinat@hey.com>
2024-07-23 12:58:09 +0200

    fix: remove comments

--- extension/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx.args.mjs
+++ extension/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx.args.mjs
@@ -1,4 +1,2 @@
 export const providerNames = "OnchainKitScaffoldProvider";
-// export const providerSetups = "";
 export const providerImports = `import { OnchainKitScaffoldProvider } from "./OnchainKitScaffoldProvider";`;
-// export const providerProps = "";
