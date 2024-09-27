7b8281866ada20ee87eb42b0c7dcc78708ee224d
Shiv Bhonde <shivbhonde04@gmail.com>
2024-09-27 21:58:27 +0530

    update example with latest foundry changes

--- extension/packages/foundry/script/Deploy.s.sol.args.mjs
+++ extension/packages/foundry/script/Deploy.s.sol.args.mjs
@@ -1,11 +1,5 @@
-export const deploymentsScriptsImports = `import "../contracts/SE2Token.sol";`;
+export const deploymentsScriptsImports = `import { DeploySE2Token } from "./DeploySE2Token.s.sol";`;
 export const deploymentsLogic = `
-
-    vm.startBroadcast(deployerPrivateKey);
-
-    SE2Token se2Token = new SE2Token();
-    console.logString(string.concat("SE2Token deployed at: ", vm.toString(address(se2Token))));
-
-    vm.stopBroadcast();
-
+    DeploySE2Token deploySE2Token = new DeploySE2Token();
+    deploySE2Token.run();
 `;
