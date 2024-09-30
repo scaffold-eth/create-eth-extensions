320bdfd17a8e96809c61845c3e747ce738cc380b
Shiv Bhonde <shivbhonde04@gmail.com>
2024-09-30 23:55:27 +0530

    update foundry deployments inline with new template

--- extension/packages/foundry/contracts/EIP5792_Example.sol
+++ extension/packages/foundry/contracts/EIP5792_Example.sol
@@ -7,17 +7,17 @@ pragma solidity >=0.8.0 <0.9.0;
  * @author BuidlGuidl
  */
 contract EIP5792_Example {
-	string public greeting = "Building Unstoppable Apps!!!";
-	bool public premium = false;
-	uint256 public totalCounter = 0;
+  string public greeting = "Building Unstoppable Apps!!!";
+  bool public premium = false;
+  uint256 public totalCounter = 0;
 
-	function setGreeting(string memory _newGreeting) public payable {
-		greeting = _newGreeting;
-	}
+  function setGreeting(string memory _newGreeting) public payable {
+    greeting = _newGreeting;
+  }
 
-	function increaseCounter() public {
-		totalCounter += 1;
-	}
+  function increaseCounter() public {
+    totalCounter += 1;
+  }
 
-	receive() external payable {}
+  receive() external payable { }
 }
--- extension/packages/foundry/script/Deploy.s.sol.args.mjs
+++ extension/packages/foundry/script/Deploy.s.sol.args.mjs
@@ -1,11 +1,5 @@
-export const deploymentsScriptsImports = `import "../contracts/EIP5792_Example.sol";`;
+export const deploymentsScriptsImports = `import { DeployEIP5792ExampleContract } from "./DeployEIP5792ExampleContract.sol";`;
 export const deploymentsLogic = `
-
-    vm.startBroadcast(deployerPrivateKey);
-
-    EIP5792_Example eip5792Example= new EIP5792_Example();
-    console.logString(string.concat("EIP5792_Example deployed at: ", vm.toString(address(eip5792Example))));
-
-    vm.stopBroadcast();
-
+    DeployEIP5792ExampleContract deployEIP5792ExampleContract = new DeployEIP5792ExampleContract();
+    deployEIP5792ExampleContract.run();
 `;
