a4a5d2443dba1bb5dca6f1b7d8ec9267e998eea3
Damian <damianmarti@gmail.com>
2024-10-01 10:39:03 -0300

    Randao random extension

---
+++ extension/README.md.args.mjs
@@ -0,0 +1,15 @@
+export const extraContents = `## 🎲 Setup RANDAO Extension
+
+This extension shows how to use on-chain randomness using RANDAO for truly on-chain unpredictable random sources.
+
+Ethereum PoS introduces randomness using block.mixHash (prevRandao). Look at [EIP-4399](https://eips.ethereum.org/EIPS/eip-4399) for more information.
+
+### Setup
+
+Deploy your contract running \`\`\`yarn deploy\`\`\`
+
+### Testing the random generator
+
+Start the front-end with \`\`\`yarn start\`\`\` and go to the _/randao_ page to get more information and interact with your deployed RandomGenerator contract.
+
+`;
---
+++ extension/packages/nextjs/components/Header.tsx.args.mjs
@@ -0,0 +1,6 @@
+export const menuIconImports = `import { CalculatorIcon } from "@heroicons/react/24/outline";`;
+export const menuObjects = `{
+  label: "RANDAO",
+  href: "/randao",
+  icon: <CalculatorIcon className="h-4 w-4" />,
+}`;
