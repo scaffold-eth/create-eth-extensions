3a80ef8fda907e5714e4ff5aab1c5b1dd28fcf0e
Shiv Bhonde <shivbhonde04@gmail.com>
2024-11-01 10:59:29 +0700

    fix minor styling issues

--- extension/packages/nextjs/app/randao/page.tsx
+++ extension/packages/nextjs/app/randao/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import type { NextPage } from "next";
-import { useBlockNumber } from "wagmi";
-import { useScaffoldReadContract, useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
 import { useEffect, useState } from "react";
+import type { NextPage } from "next";
 import { createPublicClient, http, keccak256, toHex, toRlp } from "viem";
+import { useBlockNumber } from "wagmi";
+import { BanknotesIcon } from "@heroicons/react/24/outline";
+import { useScaffoldReadContract, useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
 import scaffoldConfig from "~~/scaffold.config";
 import { notification } from "~~/utils/scaffold-eth";
 
@@ -52,7 +53,7 @@ const Randao: NextPage = () => {
         },
       },
     );
-  }
+  };
 
   const getRandomNumber = async () => {
     console.log("currentBlockNumber: ", currentBlockNumber);
@@ -135,16 +136,18 @@ const Randao: NextPage = () => {
     );
   };
 
-  const getRandomNumberDisabled = currentBlockNumber === undefined || targetBlockNumber === undefined || currentBlockNumber <= targetBlockNumber;
+  const getRandomNumberDisabled =
+    currentBlockNumber === undefined || targetBlockNumber === undefined || currentBlockNumber <= targetBlockNumber;
 
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-10">
+      <div className="flex items-center flex-col flex-grow pt-10 px-5">
         <div className="px-5 text-center max-w-4xl">
           <h1 className="text-4xl font-bold">On-chain randomness using RANDAO</h1>
           <div>
             <p>
-              This extension shows how to use on-chain randomness using RANDAO for truly on-chain unpredictable random sources.
+              This extension shows how to use on-chain randomness using RANDAO for truly on-chain unpredictable random
+              sources.
             </p>
             <p>
               Ethereum PoS introduces randomness using block.mixHash (prevRandao). Look at{" "}
@@ -152,27 +155,32 @@ const Randao: NextPage = () => {
                 target="_blank"
                 href="https://eips.ethereum.org/EIPS/eip-4399"
                 className="underline font-bold text-nowrap"
-              >EIP-4399</a>{" "}
+              >
+                EIP-4399
+              </a>{" "}
               for more information.
             </p>
-            <p className="mt-8">
-              The randomness must be generated in two steps:
+            <div className="mt-8">
+              <span className="font-semibold">The randomness must be generated in two steps:</span>
               <ol className="list-decimal">
                 <li className="mt-4">
-                  First, a block in the future must be set from where the block.mixHash value will be used as the random source.
-                  For this, the user has to call a function in our contract.
+                  First, a block in the future must be set from where the block.mixHash value will be used as the random
+                  source. For this, the user has to call a function in our contract.
                 </li>
                 <li className="mt-4">
-                  Then, after this future block is mined, the user needs to call again to get the random number, passing the whole block header as the parameter.
-                  The function uses the block header and the block hash to validate the header and use the block.mixHash value as a randomness source.
+                  Then, after this future block is mined, the user needs to call again to get the random number, passing
+                  the whole block header as the parameter. The function uses the block header and the block hash to
+                  validate the header and use the block.mixHash value as a randomness source.
                 </li>
               </ol>
+            </div>
+            <p className="mt-8">
+              This strategy can be used to generate random numbers for games, lotteries, etc., but also to generate
+              random traits for NFTs, for example, avoiding centralized oracles.
             </p>
             <p className="mt-8">
-              This strategy can be used to generate random numbers for games, lotteries, etc., but also to generate random traits for NFTs, for example, avoiding centralized oracles.
-            </p>
-            <p className="mt-8">
-              <em>Foundry Note:</em> if you are using Anvil from Foundry, the random number will always be the same because Anvil always sets mixHash to zero.
+              <em>Foundry Note:</em> if you are using Anvil from Foundry, the random number will always be the same
+              because Anvil always sets mixHash to zero.
             </p>
           </div>
 
@@ -181,47 +189,51 @@ const Randao: NextPage = () => {
           <h2 className="text-3xl font-bold mt-4">Testing the random generator</h2>
 
           <div>
-            <p>Using the button below you can call <strong>Generate Random Number</strong>.</p>
             <p>
-              The function on the contract will set a future block number (set to 2 blocks into the future) to generate the random number.
+              Using the button below you can call <strong>Generate Random Number</strong>.
             </p>
             <p>
-              After this block is mined, you can call the <strong>Get Random Number</strong> button to get the random number.
+              The function on the contract will set a future block number (set to 2 blocks into the future) to generate
+              the random number.
             </p>
+            <div className="flex flex-col">
+              <p className="my-0">
+                After this block is mined, you can call the <strong>Get Random Number</strong> button to get the random
+                number.
+              </p>
+              <div className="my-0">
+                (TIP: You can click the <BanknotesIcon className="h-4 w-4 inline" /> in top right corner to mine new
+                blocks)
+              </div>
+            </div>
           </div>
         </div>
 
         <div className="flex-grow bg-base-300 w-full mt-8 px-8 pt-6 pb-12">
           <p className="text-center text-lg">
-            <button
-              className="btn btn-primary mt-2"
-              onClick={generateRandomNumber}
-            >
+            <button className="btn btn-primary dark:btn-secondary mt-2" onClick={generateRandomNumber}>
               Generate Random Number
             </button>
           </p>
-          <p className="text-center text-lg">
-            Current Block Number: {currentBlockNumber?.toString()}
-          </p>
+          <p className="text-center text-lg">Current Block Number: {currentBlockNumber?.toString()}</p>
           {targetBlockNumber && (
-            <p className="text-center text-lg">
-              Waiting for Block Number: {(targetBlockNumber + 1n).toString()}
-            </p>
+            <p className="text-center text-lg">Waiting for Block Number: {(targetBlockNumber + 1n).toString()}</p>
           )}
           <p className="text-center text-lg">
             <button
-              className="btn btn-primary mt-2"
+              className="btn btn-primary dark:btn-secondary mt-2"
               onClick={getRandomNumber}
               disabled={getRandomNumberDisabled}
             >
               Get Random Number
             </button>
           </p>
-          <p className="text-center text-lg">
-            Random Number: <strong>{randomNumber?.toString()}</strong>
-          </p>
+          <div className="text-center text-lg break-all">
+            <p className="my-0">Random Number:</p>
+            <strong>{randomNumber?.toString()}</strong>
+          </div>
         </div>
-      </div >
+      </div>
     </>
   );
 };
