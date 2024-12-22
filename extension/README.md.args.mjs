4d5e01a16a161d59f8a4068bc153a2ecf291786b
Pablo <55535804+Pabl0cks@users.noreply.github.com>
2024-12-22 01:05:56 +0100

    Fix EIP link in template

--- extension/README.md.args.mjs
+++ extension/README.md.args.mjs
@@ -2,7 +2,7 @@ export const extraContents = `## 🚀 Setup ERC-721 NFT Extension
 
 This extension introduces an ERC-721 token contract and demonstrates how to use it, including getting the total supply and holder balance, listing all NFTs from the collection and NFTs from the connected address, and how to transfer NFTs.
 
-The ERC-721 Token Standard introduces a standard for Non-Fungible Tokens ([EIP-721](https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol)), in other words, each token is unique.
+The ERC-721 Token Standard introduces a standard for Non-Fungible Tokens ([EIP-721](https://eips.ethereum.org/EIPS/eip-721)), in other words, each token is unique.
 
 The ERC-721 token contract is implemented using the [ERC-721 token implementation](https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol) from OpenZeppelin.
 
