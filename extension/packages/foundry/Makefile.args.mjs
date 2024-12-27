caef5204f0e6691cddbc68d348e2ba5c808c484c
Rinat <rinat@hey.com>
2024-12-27 14:40:02 +0100

    feat: foundry makefile

---
+++ extension/packages/foundry/Makefile.args.mjs
@@ -0,0 +1,7 @@
+export const recipes = `
+# Example extension postdeploy recipe
+example-extension-postdeploy-recipe:
+	echo "Example extension postdeploy recipe"
+`;
+
+export const postDeployRecipeToRun = "example-extension-postdeploy-recipe";
