288a9f7eeb4c3d5fd67fea352ce2d016df041957
Shiv Bhonde <shivbhonde04@gmail.com>
2025-01-02 13:05:02 +0530

    add foundry.toml.args file

---
+++ extension/packages/foundry/foundry.toml.args.mjs
@@ -0,0 +1,14 @@
+export const profileDefaults = `via_ir = true`;
+export const rpcEndpoints = `
+myFakeNetwork = "https://my-fake-network.alchemyapi.io/v2/\${ALCHEMY_API_KEY}"
+myFakeNetwork2 = "https://my-fake-network2.alchemyapi.io/v2/\${ALCHEMY_API_KEY}"
+`;
+
+export const etherscan = `base = { key = "\${BASESCAN_API_KEY}" }`;
+
+export const fmt = `wrap_comments = true`;
+
+export const extraConfig = `
+[doc]
+title = "SE-2 Foundry"
+`;
