2d7d6917a63622df7997ae8558b972723c0dbb4a
Shiv Bhonde <shivbhonde04@gmail.com>
2025-01-03 19:09:40 +0530

    rename vars

--- extension/packages/foundry/foundry.toml.args.mjs
+++ extension/packages/foundry/foundry.toml.args.mjs
@@ -1,12 +1,12 @@
-export const profileDefaults = `via_ir = true`;
-export const rpcEndpoints = `
+export const extraProfileDefaults = `via_ir = true`;
+export const extraRpcEndpoints = `
 myFakeNetwork = "https://my-fake-network.alchemyapi.io/v2/\${ALCHEMY_API_KEY}"
 myFakeNetwork2 = "https://my-fake-network2.alchemyapi.io/v2/\${ALCHEMY_API_KEY}"
 `;
 
-export const etherscan = `base = { key = "\${BASESCAN_API_KEY}" }`;
+export const extraEthercsanConfig = `base = { key = "\${BASESCAN_API_KEY}" }`;
 
-export const fmt = `wrap_comments = true`;
+export const extraFormattingConfig = `wrap_comments = true`;
 
 export const extraConfig = `
 [doc]
