1f776180638b14d814eb1071b036e12e98d89df4
Damian Martinelli <damianmarti@gmail.com>
2025-02-03 12:01:46 -0400

    Update Ponder extension to Ponder v0.8 (#48)

--- extension/packages/ponder/ponder.config.ts
+++ extension/packages/ponder/ponder.config.ts
@@ -1,4 +1,4 @@
-import { createConfig } from "@ponder/core";
+import { createConfig } from "ponder";
 import { http } from "viem";
 import deployedContracts from "../nextjs/contracts/deployedContracts";
 import scaffoldConfig from "../nextjs/scaffold.config";
