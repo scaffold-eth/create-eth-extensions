4df32faa266e0167dd2180ac6f3fd66466d4528e
Shiv Bhonde <shivbhonde04@gmail.com>
2025-03-12 12:04:37 +0100

    add custom chain example

--- extension/packages/nextjs/scaffold.config.ts.args.mjs
+++ extension/packages/nextjs/scaffold.config.ts.args.mjs
@@ -1,1 +1,20 @@
-export const chainName = 'sepolia';
+export const extraConfigTypeName = "ExtraConfig";
+
+export const preConfigContent = `
+import { customChain } from "./utils/customChain";
+
+// Custom variables
+const CUSTOM_API_KEY = process.env.CUSTOM_API_KEY;
+
+export type ${extraConfigTypeName} = {
+  // Random comment
+  customApiKey: string | undefined;
+}
+`;
+
+export const configOverrides = {
+  targetNetworks: ["$$customChain$$"],
+  pollingInterval: 12_345,
+  onlyLocalBurnerWallet: false,
+  customApiKey: "$$CUSTOM_API_KEY$$",
+};
