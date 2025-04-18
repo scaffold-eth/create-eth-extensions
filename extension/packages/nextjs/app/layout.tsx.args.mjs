5e3af4bdc7f1a994347eb1af3d49230ef77c9dca
Shiv Bhonde <shivbhonde04@gmail.com>
2025-04-18 11:37:30 +0530

    update args for Header

--- extension/packages/nextjs/app/layout.tsx.args.mjs
+++ extension/packages/nextjs/app/layout.tsx.args.mjs
@@ -1,6 +1,6 @@
 export const imports = 'import "../styles/font-color.css";';
 
-export const metadata = {
+export const metadataOverrides = {
   title: "Scaffold-ETH 2 Example Extension App (ERC-20)",
   description: "Built with 🏗 Scaffold-ETH 2",
 };
