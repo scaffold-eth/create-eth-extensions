4feeb07291bc6d4381aa5bf4a1643d643fd1bbce
Shiv Bhonde <shivbhonde04@gmail.com>
2025-05-06 14:01:36 +0530

    update ScaffoldEthAppWithProvider args

--- extension/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx.args.mjs
+++ extension/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx.args.mjs
@@ -1,9 +1,21 @@
-export const providerNames = "ExampleContext.Provider";
 export const preConfigContent = `
   import { createContext } from 'react';
   export const ExampleContext = createContext("");
 `;
-export const providerProps = "value='Example'";
 export const globalClassNames = "font-space-grotesk";
 
+export const extraProviders = [
+  '$$createProvider(ExampleContext.Provider, {value: "Example"})$$'
+]
+
+// WARNING: Do it with caution
+// Since SE-2 fundamentally relies on WagmiProvider, QueryClientProvider, ProgressBar and RainbowKitProvider don't forgot to include them
+// overrideProviders allows you to change the order or props passed to default providers
+/* export const overrideProviders = [
+  '$$createProvider(WagmiProvider, { config: wagmiConfig })$$',
+  '$$createProvider(ExampleContext.Provider, {value: "Example"})$$',
+  '$$createProvider(QueryClientProvider, { client: queryClient })$$',
+  '$$createProvider(ProgressBar, { height: "3px", color: "#2299dd" })$$',
+  '$$createProvider(RainbowKitProvider, { avatar: BlockieAvatar, theme: mounted ? (isDarkMode ? darkTheme() : lightTheme()) : lightTheme() })$$',
+] */
 
