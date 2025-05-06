2ef372d4cd26788251cfbabf88f5f70da4a6cfe7
Shiv Bhonde <shivbhonde04@gmail.com>
2025-05-06 18:25:35 +0530

    add example of wagmiConfig

---
+++ extension/packages/nextjs/services/web3/wagmiConfig.tsx.args.mjs
@@ -0,0 +1,21 @@
+export const preConfigContent = `
+import { arbitrum } from "viem/chains";
+`
+
+export const configOverrides = {
+  client: `$$({chain})=>{const alchemyHttpUrl=chain.id==(arbitrum.id as number)?"http://127.0.0.1:8545":getAlchemyHttpUrl(chain.id);const rpcFallbacks=alchemyHttpUrl?[http(),http(alchemyHttpUrl)]:[http()];return createClient({chain,transport:fallback(rpcFallbacks)})}$$`
+}
+
+// NOTE: While passing function you need to ensure it's in one line. 
+// You can use AI to format the code to one line. (check above example for reference)
+/* export const configOverrides = {
+  client: `$$({ chain }) => {
+    const alchemyHttpUrl = chain.id == arbitrum.id ? "http://127.0.0.1:8545" : getAlchemyHttpUrl(chain.id);
+    const rpcFallbacks = alchemyHttpUrl ? [http(), http(alchemyHttpUrl)] : [http()];
+
+    return createClient({
+      chain,
+      transport: fallback(rpcFallbacks),
+    });
+  }$$`
+} */
