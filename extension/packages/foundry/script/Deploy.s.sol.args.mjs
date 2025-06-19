48bd9bfff78e1cad1d42b1fe3f8e359feac4c683
Shiv Bhonde <shivbhonde04@gmail.com>
2025-06-19 21:52:25 +0530

    inline extension with beta

--- extension/packages/foundry/script/Deploy.s.sol.args.mjs
+++ extension/packages/foundry/script/Deploy.s.sol.args.mjs
@@ -1,4 +1,4 @@
-export const deploymentsScriptsImports = `import { DeploySE2Nft } from "./DeploySE2Nft.s.sol";`;
+export const preContent = `import { DeploySE2Nft } from "./DeploySE2Nft.s.sol";`;
 export const deploymentsLogic = `
     DeploySE2Nft deploySE2Nft = new DeploySE2Nft();
     deploySE2Nft.run();
--- extension/packages/nextjs/components/Header.tsx.args.mjs
+++ extension/packages/nextjs/components/Header.tsx.args.mjs
@@ -1,6 +1,8 @@
-export const menuIconImports = `import { PhotoIcon } from "@heroicons/react/24/outline";`;
-export const menuObjects = `{
-  label: "ERC-721",
-  href: "/erc721",
-  icon: <PhotoIcon className="h-4 w-4" />,
-}`;
+export const preContent = `import { PhotoIcon } from "@heroicons/react/24/outline";`;
+export const menuObjects = [
+  {
+    label: "ERC-721",
+    href: "/erc721",
+    icon: '$$<PhotoIcon className="h-4 w-4" />$$',
+  },
+];
