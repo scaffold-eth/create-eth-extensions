e83f8629e48ec1ded3b3aeee661b608274ee9502
Shiv Bhonde | shivbhonde.eth <shivbhonde04@gmail.com>
2025-06-19 21:51:56 +0530

    Discard changes to extension/packages/foundry/contracts/SE2Token.sol

--- extension/packages/foundry/contracts/SE2Token.sol
+++ extension/packages/foundry/contracts/SE2Token.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SE2Token is ERC20 {
-    constructor() ERC20("SE2Token", "SE2") { }
+    constructor() ERC20("SE2Token", "SE2") {}
 
     // Minting is open to anyone and for free.
     // You can implement your custom logic to mint tokens.
