26f3e122b9b5c9cc8907e59d7b2a32ca6996417c
Shiv Bhonde | shivbhonde.eth <shivbhonde04@gmail.com>
2025-07-10 15:50:27 +0530

    inline extension with beta (#75)

--- extension/packages/nextjs/components/Header.tsx.args.mjs
+++ extension/packages/nextjs/components/Header.tsx.args.mjs
@@ -1,6 +1,8 @@
-export const menuIconImports = `import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";`;
-export const menuObjects = `{
-  label: "Ponder",
-  href: "/ponder-greetings",
-  icon: <MagnifyingGlassIcon className="h-4 w-4" />,
-}`;
+export const preContent = `import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";`;
+export const extraMenuLinksObjects = [
+  {
+    label: "Ponder",
+    href: "/ponder-greetings",
+    icon: '$$<MagnifyingGlassIcon className="h-4 w-4" />$$',
+  },
+];
