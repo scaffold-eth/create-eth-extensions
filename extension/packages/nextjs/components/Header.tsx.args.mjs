4cb6d9c0d77d8c3b4fd366848e8002bcb9f9e870
Shiv Bhonde | shivbhonde.eth <shivbhonde04@gmail.com>
2025-07-10 15:50:03 +0530

    (beta) ERC-20 Token Extension (#17) (#73)

--- extension/packages/nextjs/components/Header.tsx.args.mjs
+++ extension/packages/nextjs/components/Header.tsx.args.mjs
@@ -1,6 +1,8 @@
-export const menuIconImports = `import { BanknotesIcon } from "@heroicons/react/24/outline";`;
-export const menuObjects = `{
-  label: "ERC-20",
-  href: "/erc20",
-  icon: <BanknotesIcon className="h-4 w-4" />,
-}`;
+export const preContent = `import { BanknotesIcon } from "@heroicons/react/24/outline";`;
+export const extraMenuLinksObjects = [
+  {
+    label: "ERC-20",
+    href: "/erc20",
+    icon: '$$<BanknotesIcon className="h-4 w-4" />$$',
+  },
+];
