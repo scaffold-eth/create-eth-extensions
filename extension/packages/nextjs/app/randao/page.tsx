687ac85f38b3d298c535516bf8b88628ae34ad24
Damian Martinelli <damianmarti@gmail.com>
2025-09-02 09:01:31 -0300

    Update Randao extension with new requestsHash header (#99)
    * Add new requestsHash header
    
    * Use new useScaffoldWriteContract params
    
    * Remove debug console.log
    
    * Use toHex on blockData values

--- extension/packages/nextjs/app/randao/page.tsx
+++ extension/packages/nextjs/app/randao/page.tsx
@@ -18,7 +18,7 @@ const Randao: NextPage = () => {
     transport: http(),
   });
 
-  const { writeContractAsync: writeAsync } = useScaffoldWriteContract("RandomGenerator");
+  const { writeContractAsync: writeAsync } = useScaffoldWriteContract({ contractName: "RandomGenerator" });
 
   const { data: futureBlocks } = useScaffoldReadContract({
     contractName: "RandomGenerator",
@@ -61,12 +61,7 @@ const Randao: NextPage = () => {
 
   const getRandomNumber = async () => {
     try {
-      console.log("currentBlockNumber: ", currentBlockNumber);
-      console.log("targetBlockNumber: ", targetBlockNumber);
-
       const blockData = await publicClient.getBlock({ blockNumber: targetBlockNumber });
-
-      console.log("blockData: ", blockData);
 
       const values: `0x${string}`[] = [];
       values.push(blockData.parentHash);
@@ -76,16 +71,16 @@ const Randao: NextPage = () => {
       values.push(blockData.transactionsRoot);
       values.push(blockData.receiptsRoot);
       values.push(blockData.logsBloom);
-      values.push(`0x${blockData.difficulty.toString(16)}`);
-      values.push(`0x${blockData.number.toString(16)}`);
-      values.push(`0x${blockData.gasLimit.toString(16)}`);
-      values.push(`0x${blockData.gasUsed.toString(16)}`);
-      values.push(`0x${blockData.timestamp.toString(16)}`);
+      values.push(toHex(blockData.difficulty));
+      values.push(toHex(blockData.number));
+      values.push(toHex(blockData.gasLimit));
+      values.push(toHex(blockData.gasUsed));
+      values.push(toHex(blockData.timestamp));
       values.push(blockData.extraData);
       values.push(blockData.mixHash);
       values.push(blockData.nonce);
       if ("baseFeePerGas" in blockData && blockData.baseFeePerGas !== null) {
-        values.push(`0x${blockData.baseFeePerGas.toString(16)}`);
+        values.push(toHex(blockData.baseFeePerGas));
       }
       if ("withdrawalsRoot" in blockData && blockData.withdrawalsRoot !== undefined) {
         values.push(blockData.withdrawalsRoot);
@@ -106,7 +101,10 @@ const Randao: NextPage = () => {
         values.push(blockData.parentBeaconBlockRoot as `0x${string}`);
       }
 
-      console.log("blockData values: ", values);
+      if ("requestsHash" in blockData && blockData.requestsHash !== undefined && blockData.requestsHash !== null) {
+        values.push(blockData.requestsHash as `0x${string}`);
+      }
+
       for (let i = 0; i < values.length; i++) {
         if (values[i] === "0x0") {
           values[i] = "0x";
@@ -115,13 +113,9 @@ const Randao: NextPage = () => {
           values[i] = ("0x0" + values[i].substring(2)) as `0x${string}`;
         }
       }
-      console.log("blockData values after: ", values);
 
       const rlpEncodedValues = toRlp(values);
-      console.log("blockData RLP: ", rlpEncodedValues);
-
       const blockHash = keccak256(rlpEncodedValues);
-      console.log("blockData hash: ", blockHash);
 
       if (blockHash !== blockData.hash) {
         notification.error("Block hash mismatch");
