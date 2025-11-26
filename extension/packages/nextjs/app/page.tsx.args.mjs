a358561e996402b86d970a039b316c7dda865b99
Rinat <rinat@hey.com>
2025-11-26 20:45:20 +0400

    feat: pass global vars (#104)

--- extension/packages/nextjs/app/page.tsx.args.mjs
+++ extension/packages/nextjs/app/page.tsx.args.mjs
@@ -1,9 +1,9 @@
 export const preContent = `import { RocketLaunchIcon } from '@heroicons/react/24/outline'`;
 
-export const description = `
+export const description = ({ solidityFramework }) => `
 <div className="flex mt-4 bg-green-100 gap-2 p-4 justify-center items-center rounded-lg">
   <RocketLaunchIcon className="h-8 w-8 fill-green-100" />
-  <p>Your description block</p>
+  <p>ERC-20 extension example using ${solidityFramework}</p>
 </div>
 `;
 
