143aff7c5939e0dded41a5a5b3881d13ba93b10e
Damian <damianmarti@gmail.com>
2025-12-16 17:20:19 -0300

    Upgrade Ponder to 0.15.17

---
+++ extension/packages/ponder/src/api/index.ts
@@ -0,0 +1,12 @@
+import { db } from "ponder:api";
+import schema from "ponder:schema";
+import { Hono } from "hono";
+import { graphql } from "ponder";
+
+const app = new Hono();
+
+app.use("/", graphql({ db, schema }));
+app.use("/graphql", graphql({ db, schema }));
+
+export default app;
+
